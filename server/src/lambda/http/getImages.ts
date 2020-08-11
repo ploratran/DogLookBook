6d5af41d4a0d1f4ed3e43fb7adce3bcd1b2101c5
Plora Tran <ploratran@gmail.com>
2020-08-10 18:38:22 -0700

    set cors header in getImages.ts but cannot fix invalid mapping expression error

--- server/src/lambda/http/getImages.ts
+++ server/src/lambda/http/getImages.ts
@@ -21,6 +21,10 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
 
     return {
         statusCode: 200, 
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true
+        },
         body: JSON.stringify({
             items: images, 
         })
