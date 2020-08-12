4f4f1152d581af4a539c238b391cd948a875f976
Plora Tran <ploratran@gmail.com>
2020-08-11 19:42:46 -0700

    try to move iamRole x-ray tracing to provider but still tracing disabled

--- server/src/auth/Jwt.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// interface of JWT token with Header and Payload
-exports.__esModule = true;
-//# sourceMappingURL=Jwt.js.map
--- server/src/auth/JwtPayload.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// interface for JWT token payload: 
-exports.__esModule = true;
-//# sourceMappingURL=JwtPayload.js.map
