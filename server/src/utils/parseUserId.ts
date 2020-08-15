909f4f997d5be877fe63ba0a6cd5138b5cf08971
Plora Tran <ploratran@gmail.com>
2020-08-14 20:18:18 -0700

    successfully implement GET with fake data in DynamoDB

--- server/src/utils/parseUserId.ts
+++ server/src/utils/parseUserId.ts
@@ -1,12 +1,16 @@
 import { decode } from 'jsonwebtoken'; 
 import { JwtPayload } from '../auth/JwtPayload'; 
+import { createLogger } from './logger'; 
 
 // parse JWT token and return userId
 // @params jwtToken
 // @return userId from JWT token
 
+const logger = createLogger('parseUserId.ts'); 
+
 export function parseUserId(jwtToken: string): string {
-    const decodedJwt = decode(jwtToken) as JwtPayload; 
+    const decodedJwt = decode(jwtToken) as JwtPayload;
+    logger.info('sub ', decodedJwt.sub);  
     return decodedJwt.sub; 
 }
   
