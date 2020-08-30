364111ef623c30ad65f4ddfadd6f09c994741294
Plora Tran <ploratran@gmail.com>
2020-08-29 23:41:08 -0700

    success fix Cannot get sub of null error in API Gateway

--- server/src/utils/parseUserId.ts
+++ server/src/utils/parseUserId.ts
@@ -1,21 +1,14 @@
 import { decode } from 'jsonwebtoken'; 
 import { JwtPayload } from '../auth/JwtPayload'; 
-import { createLogger } from '../utils/logger'; 
 /**
  * parse JWT token and return userId
  * @param jwtToken 
  * @return userId from JWT token
 */
 
-const logger = createLogger('parseUserId.ts'); 
-
-export const parseUserId = (jwtToken: string): string => {
-    logger.info(`jwt token ${jwtToken}`);
+export function parseUserId(jwtToken: string): string {
     const decodedJwt = decode(jwtToken) as JwtPayload;
-    logger.info(`decodedJwt ${decodedJwt}`); 
-    if (decodedJwt.sub !== null) {
-        return decodedJwt.sub ; // user id from Auth0
-    }
+    return decodedJwt.sub ; // user id from Auth0
 }
   
   
